fd21badea8545817d68ec03bb40aaeed4e07b175
GeoSauer <geoffrey.sauer89@gmail.com>
2022-09-09 17:17:01 -0600

    forgot to complete previous commit so these might be the same

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const inputContainer = document.getElementById('input-container');
 const fortuneContainer = document.getElementById('fortune-container');
 
 askButton.addEventListener('click', () => {
+    const randNum = Math.floor(Math.random() * answers.length);
+    fortune.textContent = answers[randNum];
+
     inputContainer.classList.toggle('hide');
     fortuneContainer.classList.toggle('hide');
 });
@@ -13,3 +16,27 @@ resetButton.addEventListener('click', () => {
     inputContainer.classList.toggle('hide');
     fortuneContainer.classList.toggle('hide');
 });
+
+const fortune = document.getElementById('fortune');
+
+const answers = [
+    'Yes, definitely',
+    'It is certain',
+    'It is decidedly so',
+    'Without a doubt',
+    'You may rely on it',
+    'As I see it, yes',
+    'Most Likely',
+    'Outlook good',
+    'Signs point to yes',
+    'Reply hazy, try again',
+    'Ask again later',
+    'Better not tell you now',
+    'Cannot predict now',
+    'Concentrate and ask again',
+    'Don’t count on it',
+    'My reply is no',
+    'My sources say no',
+    'Outlook not so good',
+    'Very doubtful',
+];
